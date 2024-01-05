f18239abeee297e3e868418e5d7b5fa9b62e4aa3
Randson Marinho <randsonmarinho22@gmail.com>
2024-01-05 00:24:11 -0300

    Desafio POO.net DIO
    Conclusão do desafio da DIO sobre POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i = new Iphone("123", "Iphone", "174950056345530", 60);
+i.InstalarAplicativo("Terraria");
+
+Nokia n = new Nokia("235", "Nokia", "345653637898787", 40);
+n.InstalarAplicativo("Minecraft");
+
+
+i.Ligar();
+n.ReceberLigacao();
